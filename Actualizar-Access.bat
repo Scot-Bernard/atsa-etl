ecee8e21a2b267e734cca1e7497067d018f68b1e
Matias Sanchez <matias.sanchez@scotsteam.com.ar>
2016-06-02 15:14:54 -0300

    initial commit

---
+++ Actualizar-Access.bat
@@ -0,0 +1,2 @@
+Set PENTAHO_JAVA_HOME=C:\ATSA-ACCESS\jre
+C:\ATSA-ACCESS\data-integration\Kitchen.bat -file=C:\ATSA-ACCESS\ATSA-ACCESS.kjb
---
+++ Spoon.bat
@@ -0,0 +1,2 @@
+Set PENTAHO_JAVA_HOME=C:\Projects\ATSACampo\ATSA-ACCESS\jre
+C:\Projects\ATSACampo\ATSA-ACCESS\data-integration\Spoon.bat
